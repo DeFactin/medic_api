492e9c7ef48bb2b6ded26ce6b768f04ef4a6c413
DeFactin <aminadefacto47@gmail.com>
2024-07-30 09:56:16 +0200

    Username and Password Validation

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema
+const validator = require('validator')
 
 const userSchema = new Schema({
     id: {
@@ -50,11 +51,21 @@ const userSchema = new Schema({
 // signup method
 userSchema.statics.signup = async function (username, password, name, orders, image, birthdate) {
 
-    const exists = await this.findOne({ username })
 
+    //validation
+    if (!username || !password || !name || !orders || !image || !birthdate) {
+        throw Error('All fields must be filled')
+    }
+
+    const exists = await this.findOne({ username })
     if (exists) {
         throw Error('Username already in use')
     }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+
 
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
