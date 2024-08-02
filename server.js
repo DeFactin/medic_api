ec77ecf04c2314ba81b5a52a9de5523a9b8c6470
DeFactin <aminadefacto47@gmail.com>
2024-08-02 14:05:56 +0200

    Cors Issue Update

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const cors = require('cors')
 // express app
 const app = express()
 app.use(cors({
-  origin: ["https://mediclab-web.vercel.app/"],
-  methods: ["POST", "GET"],
-  credentials: true
+  origin: 'https://mediclab-web.vercel.app/', // Replace with your frontend URL
+  methods: 'GET,POST,PUT,DELETE', // Allow necessary methods
+  credentials: true // If you need to send cookies or auth headers
 }));
 // middleware
 app.use(express.json())
