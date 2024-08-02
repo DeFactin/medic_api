7fd0604c7f780c756da83abd3abc9b5e06700dbb
DeFactin <aminadefacto47@gmail.com>
2024-08-02 12:46:00 +0200

    Vercel

--- server.js
+++ server.js
@@ -3,32 +3,34 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const usersRoutes = require('./routes/users')
-const loginRoute = require ('./routes/login')
-const logoutRoute = require ('./routes/logout')
-const registerUser = require ('./routes/register')
+const loginRoute = require('./routes/login')
+const logoutRoute = require('./routes/logout')
+const registerUser = require('./routes/register')
 
 const cors = require('cors')
 // express app
 const app = express()
 app.use(cors({
-  origin: '*'
+  origin: ["https://deploy-mern-medic-api.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
 }));
 // middleware
 app.use(express.json())
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-  })
-  
+  console.log(req.path, req.method)
+  next()
+})
+
 //routes
-app.use('/api/users',usersRoutes)
+app.use('/api/users', usersRoutes)
 
-app.use('/api/login',loginRoute)
+app.use('/api/login', loginRoute)
 
-app.use('/api/register',registerUser)
+app.use('/api/register', registerUser)
 
-app.use('/api/logout',logoutRoute)
+app.use('/api/logout', logoutRoute)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
